fc91cfe4d2e3651e51e6d695977ee68794d2a468
rhubark <56987370+rhubark@users.noreply.github.com>
2020-12-26 22:17:51 -0300

    Update script.js

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ let food = {
 
 function criarBG(){
     context.fillStyle = "lightgreen";
-    context.fillRect(0, 0 ,17 * box, 17 * box);
+    context.fillRect(0, 0 ,16 * box, 16 * box);
 }
 
 function criarCobrinha(){
@@ -40,9 +40,9 @@ function update(event){
 
 function inicarJogo(){
     if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
-    if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
+    if(snake[0].x < 0 && direction == "left") snake[0].x = 15 * box;
     if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
-    if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+    if(snake[0].y < 0 && direction == "up") snake[0].y = 15 * box;
 
     for(i = 1; i < snake.length; i++){
         if(snake[0].x == snake[i].x && snake[0].y == snake [i].y){
@@ -80,4 +80,4 @@ function inicarJogo(){
     snake.unshift(newHead);
 }
 
-let jogo = setInterval(inicarJogo, 150)
+let jogo = setInterval(inicarJogo, 100)
